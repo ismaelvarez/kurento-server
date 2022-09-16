2350cf0ba9347e9ef228812f63fd885fadc304b4
Ismael Álvarez Candelario <ismaelvarez96@gmail.com>
2022-09-16 07:01:20 +0100

    Returning Map with Kurento Modules. Changed to Java 8

--- src/main/java/org/gtc/kurentoserver/services/restful/ProcessingController.java
+++ src/main/java/org/gtc/kurentoserver/services/restful/ProcessingController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController("ProcessingController")
 public class ProcessingController {
@@ -19,11 +21,13 @@ public class ProcessingController {
 
     @GetMapping("/processing/modules")
     @ResponseBody
-    List<String> getModules(@RequestHeader(value="x-access-token") String sessionId) {
+    Map<String, String> getModules(@RequestHeader(value="x-access-token") String sessionId) {
         log.trace("ProcessingController::getModules()");
         if (!sessions.sessionAlive(sessionId))
             throw new NotAuthenticatedException();
-
-        return Arrays.asList("vehicleObjectDetection", "imageRecorder");
+        Map<String, String> map = new HashMap<>();
+        map.put("Vehicle Object Detection", "vehicleObjectDetection");
+        map.put("Image Recorder", "imageRecorder");
+        return map;
     }
 }
